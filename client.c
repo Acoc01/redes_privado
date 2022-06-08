3d924fc7304994ea5034caee69cd40802cede2b2
Acoc01 <63213116+Acoc01@users.noreply.github.com>
2022-06-07 23:30:43 -0400

    Add files via upload

--- client.c
+++ client.c
@@ -4,72 +4,78 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-void send_file(FILE *fp, int sockfd);
+struct data_t {
+  char name[1024];
+  size_t size;
+};
 
-int main(){
-    char *ip = "127.0.0.1";
-    int port = 65432;
-    int e;
+void send_file(FILE *fp, int sockfd, struct data_t *datos);
 
-    int sockfd;
-    struct sockaddr_in server_addr;
+int main() {
+  char *ip = "127.0.0.1";
+  int port = 65432;
+  int e;
 
-    FILE *fp;
-    char filename[1024];
-    int filesize = 0;
-    fp = fopen(filename, "r");
-    if(fp == NULL){
-        printf("[-] Error al abrir archivo\n");
-        return;
-    }
-    fseek(fp, 0L, SEEK_END);
-    filesize = ftell(fp);
-    rewind(fp);
+  int sockfd;
+  struct sockaddr_in server_addr;
 
-    printf("[+] Ingrese nombre del archivo a enviar\n");
-    scanf("%s",filename);
-    
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sockfd < 0){
-        printf("[-] Error al crear socket\n");
-        return;
-    }
-    
-    server_addr.sin_family = AF_INET;
-    server_Addr.sin_port = port;
-    server_addr.sin_addr.s_addr = inet_addr(ip);
-
-    e = connect(sockfd, (strict sockaddr*)&server_addr, sizeof(server_addr));
-    if(e == -1){
-        printf("[-] Error al conectar\n");
-        return;
-    }
-    if(send(sockfd, filename, sizeof(filename), 0) == -1){
-        printf("[-] Error al enviar nombre de archivo\n");
-        return;
-    }
-    if(send(sockfd, filesize, sizeof(filesize), 0) == -1){
-        printf("[-] Error al enviar tamanio de archivo\n");
-        return;
-    }
-    send_file(fp, sockfd);
-    close(sockfd);
-    printf("[+] Conexion cerrada\n");
-
+  FILE *fp;
+  struct data_t datos;
+  printf("Ingrese nombre del archivo a enviar\n");
+  scanf("%s", datos.name);
+  fp = fopen(datos.name, "r");
+  if (fp == NULL) {
+    printf("[-] Error al abrir archivo\n");
     return 0;
+  }
+  fseek(fp, 0L, SEEK_END);
+  datos.size = ftell(fp);
+  rewind(fp);
+
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) {
+    printf("[-] Error al crear socket\n");
+    return 0;
+  }
+
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = port;
+  server_addr.sin_addr.s_addr = inet_addr(ip);
+
+  e = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+  if (e == -1) {
+    printf("[-] Error al conectar\n");
+    return 0;
+  }
+  send_file(fp, sockfd, &datos);
+  close(sockfd);
+  printf("[+] Conexion cerrada\n");
+
+  return 0;
 }
 
+int send_all(int socket, void *buffer, size_t length) {
+  char *ptr = (char *)buffer;
+  while (length > 0) {
+    int i = send(socket, ptr, length, 0);
+    if (i < 1)
+      return -1;
+    ptr += i;
+    length -= i;
+  }
+  return 1;
+}
 
-void send_file(FILE *fp, int sockfd, int size){
-    char data[1024];
-    double cont = 0;
-    while(fgets(data, 1, fp) != NULL){
-        if(send(sockfd, data, sizeof(data), 0) == -1){
-            printf("[-] Error al enviar archivo\n");
-            return;
-        }
-        printf("[+] %lf porciento enviado...\n",(cont/size)*100);
-        bzero(data, 1024);
-        cont++;
-    }
+void send_file(FILE *fp, int sockfd, struct data_t *datos) {
+  char *data = malloc(datos->size);
+  double cont = 0;
+  if (send_all(sockfd, datos, sizeof(*datos)) == -1) {
+    printf("[-] Error al enviar nombre y tamanio de archivo");
+    return;
+  }
+  fread(data, 1, datos->size, fp);
+  if (send_all(sockfd, data, datos->size) == -1) {
+    printf("[-] Error al enviar archivo\n");
+  }
+  fclose(fp);
 }
--- server.c
+++ server.c
@@ -2,78 +2,81 @@
 #include <stdlib.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <errno.h>
 
-void recv_file(int sockfd, int size, char *name);
+struct data_t {
+  char name[1024];
+  size_t size;
+};
 
-int main(){
-    char *ip_addr = "127.0.0.1";
-    int port = 65432;
-    int e;
+void recv_file(int sockfd);
 
-    int sockfd, new_sock;
-    struct sockaddr_in server_addr, new_addr;
-    socklen_t addr_size;
-    char buffer[1024];
+int main() {
+  char *ip_addr = "127.0.0.1";
+  int port = 65432;
+  int e;
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(sockfd = 0){
-        printf("[-] Error al crear socket\n");
-        return;
-    }
-    
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = port;
-    server_addr.sin_addr = inet_addr(ip);
+  int sockfd, new_sock;
+  struct sockaddr_in server_addr, new_addr;
+  socklen_t addr_size;
+  char buffer[1024];
 
-    e = bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
-    if(e < 0){
-        printf("[-] Error al bindiar socket\n");
-        return;
-    }
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd == 0) {
+    printf("[-] Error al crear socket\n");
+    return 0;
+  }
 
-    e = listen(sockfd, 10);
-    if(e != 0){
-        printf("[-]Error al escuchar\n");
-        return;
-    }
-    addr_size = sizeof(new_addr);
-    new_sock = accept(sockfd, (struct sockaddr*)&new_addr, &addr_size);
-    int aux,filesize;
-    char filename[1024];
-    aux = recv(sockfd, filename, 1024, 0);
-    if(aux <= 0){
-        printf("[-] Error al recibir nombre de archivo\n");
-        return;
-    }
-    aux = recv(sockfd, filesize, 1024, 0);
-    if(aux <= 0){
-        printf("[-] Error al recibir tamanio del archivo\n");
-        return;
-    }
-    write_file(new_sock, filesize, filename);
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = port;
+  server_addr.sin_addr.s_addr = inet_addr(ip_addr);
+
+  e = bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+  if (e < 0) {
+    printf("[-] Error al bindiar socket\n");
+    return 0;
+  }
+
+  e = listen(sockfd, 10);
+  if (e != 0) {
+    printf("[-]Error al escuchar\n");
+    return 0;
+  }
+  addr_size = sizeof(new_addr);
+  new_sock = accept(sockfd, (struct sockaddr *)&new_addr, &addr_size);
+  recv_file(new_sock);
 }
 
-void recv_file(int sockfd, int size, char *name){
-    int n;
-    double cont = 0;
-    FILE *fp;
-    char buffer[1024];
-    fp = fopen(name, "w");
-    if(fp == NULL){
-        printf("[-] Error al crear archivo");
-        return;
-    }
+int recv_all(int socket, void *buffer, size_t length) {
+  while (length > 0) {
+    int i = recv(socket, buffer, length, 0);
+    if (i < 1)
+      return 0;
+    buffer += i;
+    length -= i;
+  }
+  return 1;
+}
 
-    while(1){
-        n = recv(sockfd, buffer, 1, 0);
-        if(n <= 0){
-            break;
-            return;
-        }
-        cont++;
-        printf("[+] %lf porciento se ha recibido...\n",(cont/size)*100));
-        fprintf(fp, "%s", buffer);
-        bzero(buffer, 1024);
-    }
+void recv_file(int sockfd) {
+  int n;
+  double cont = 0;
+  FILE *fp;
+  struct data_t data_recibida;
+  n = recv_all(sockfd, &data_recibida, sizeof(data_recibida));
+  if (n <= 0) {
+    printf("[-] Error al obtener nombre y tamanio\n");
     return;
+  }
+  char *buffer = malloc(data_recibida.size);
+  fp = fopen(data_recibida.name, "w");
+  if (fp == NULL) {
+    printf("[-] Error al crear archivo");
+    return;
+  }
+  n = recv_all(sockfd, buffer, data_recibida.size);
+  if (n <= 0)
+    return;
+  fwrite(buffer, 1, data_recibida.size, fp);
+  fclose(fp);
 }
